6c2a8d15679ba63081ea3e005e91e2ce598fd531
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-09-07 19:40:02 +0530

    basic components structures added & configured

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { StudentComponent } from './student/student.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, StudentComponent, EmployeesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'students', component: StudentComponent},
+  {path: 'employees', component: EmployeesComponent}
+];
