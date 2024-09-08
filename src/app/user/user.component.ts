c519c70ca688d233fb0184c903749da8e962c1cb
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-09-08 17:54:52 +0530

    basic structure defined for user component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
+  alreadyUser = true;
+  loginForm: FormGroup;
+  signupForm: FormGroup;
 
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.email, Validators.required, Validators.maxLength(50)]),
+      password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}$')])
+    })
+
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+      email: new FormControl('', [Validators.email, Validators.required, Validators.maxLength(50)]),
+      password: new FormControl('', [Validators.required, Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[A-Za-z\\d]{8,}$')])
+    })
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  handleUserLogin() {
+    console.log(this.loginForm.value);
+    console.log(`login form data recieved`)
+  }
 }
