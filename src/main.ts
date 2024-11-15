a392b2f08d05900f58632919aa5ed605649f1972
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-11-15 20:09:33 +0530

    interceptor registered

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // Import HttpClientModule
+import { provideHttpClient, withInterceptors } from '@angular/common/http'; // Import HttpClientModule
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes'; // Import routes from app.routes.ts
+import { authInterceptor } from './app/services/auth.interceptor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
+    provideHttpClient(withInterceptors([authInterceptor]))
   ]
 })
 .catch(err => console.error(err));
