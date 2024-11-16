28d69066f3ed09d97d500f8a3da4b58c0274334b
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-11-16 18:12:21 +0530

    dialog box added for logout confirmation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,15 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { AuthServiceService } from '../services/auth-service.service';
+import { DialogComponent } from "../shared/dialog/dialog.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, DialogComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
+  showDialog = false;
   constructor(private router: Router, private authService: AuthServiceService) {}
 
   ngOnInit(): void {
@@ -26,5 +28,18 @@ export class HeaderComponent implements OnInit {
 
   logout() {
     this.authService.logout();
+  }
+
+  confirmLogout() {
+    this.showDialog = true;
+  }
+
+  handleLogout(choice: boolean) {
+    if (choice) {
+      this.logout();
+      this.showDialog = false;
+    } else {
+      this.showDialog = false;
+    }
   }
 }
