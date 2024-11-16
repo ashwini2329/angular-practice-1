d516ad3acff4a191ee8256596fcd9256d62dc81c
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-11-16 17:55:38 +0530

    delete student functionality implemented

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Observer, catchError, of } from 'rxjs';
+import { Observer } from 'rxjs';
 import { CommonModule } from '@angular/common';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms'
 import { StudentsServiceService } from '../services/students-service.service';
-
+import { DialogComponent } from "../shared/dialog/dialog.component";
+import { deleteStudent } from '../shared/interfaces/deleteStudentInterface';
+import { LoaderComponent } from "../shared/loader/loader.component";
 @Component({
   selector: 'app-student',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, DialogComponent, LoaderComponent],
   templateUrl: './student.component.html',
   styleUrl: './student.component.scss'
 })
@@ -15,6 +17,10 @@ export class StudentComponent implements OnInit {
   studentsData: any[] = [];
   studentForm: FormGroup;
   toggleStudent: any;
+  isLoading = false;
+  showDialog = false;
+  selectedStudent: deleteStudent = { roll_no: 0, name: '' };
+  deleteStudentsArray = [];
 
   constructor(private studentsService: StudentsServiceService) {
     this.studentForm = new FormGroup({
@@ -34,42 +40,27 @@ export class StudentComponent implements OnInit {
 
   toggleStudentPage() {
     this.toggleStudent = !this.toggleStudent;
-    console.log(this.toggleStudent)
+    this.studentForm.reset();
   }
 
-  /**
-   * get All students
-   */
   getAllStudentsData() {
+    this.isLoading = true;
     const observer: Observer<any> = {
       next: (value: any) => {
         this.studentsData = value.data;
-        console.log(`studentsData - ${this.studentsData}`)
       },
       error: (err: any) => {
-        console.error('Error fetching student data:', err);
+        this.isLoading = false;
       },
       complete: () => {
-        console.log('Student data fetched successfully');
+        this.isLoading = false;
       }
     };
-
     this.studentsService.getAllStudents()
-      .pipe(
-        catchError((err) => {
-          console.error('Error fetching student data:', err);
-          return of([]);
-        })
-      )
       .subscribe(observer);
   }
 
-  /**
-   * add new student
-   */
   addStudent() {
-    console.log(`add data successful ! ${this.studentForm.value}`)
-    console.log(`form data received`);
     const studentFomData = {
       roll_no: this.studentForm.value.roll_no,
       name: this.studentForm.value.name,
@@ -78,34 +69,48 @@ export class StudentComponent implements OnInit {
       age: this.studentForm.value.age,
       address: this.studentForm.value.address
     }
-
     const observer: Observer<any> = {
       next: (value: any) => {
-        console.log(`student added succcessfully - ${value}`)
       },
       error: (err: any) => {
-        console.error('Error adding student data:', err);
       },
       complete: () => {
-        console.log('Student data added successfully');
         this.studentForm.reset();
         this.getAllStudentsData();
         this.toggleStudent = !this.toggleStudent;
       }
     };
-
     this.studentsService.handleAddStudent(studentFomData)
-      .pipe(
-        catchError((err) => {
-          console.error('Error fetching student data:', err);
-          return of([]);
-        })
-      )
       .subscribe(observer);
   }
 
-  deleteStudent(studentId: number): void {
-    console.log(`studentId -- ${studentId}`)
+  confirmDeleteStudent(student: deleteStudent): void {
+    this.showDialog = !this.showDialog;
+    this.selectedStudent.name = student.name;
+    this.selectedStudent.roll_no = student.roll_no
+  }
+
+  handleDeleteStudent(choice: boolean) {
+    if (choice) {
+      this.showDialog = false;
+      this.isLoading = true;
+      const observer: Observer<any> = {
+        next: (value: any) => {
+
+        },
+        error: (err: any) => {
+          this.getAllStudentsData
+        },
+        complete: () => {
+          this.getAllStudentsData()
+        }
+      };
+      this.studentsService.handleDeleteStudent(this.selectedStudent)
+        .subscribe(observer)
+    } else {
+      this.selectedStudent = {roll_no: 0, name: ''};
+      this.showDialog = false;
+    }
   }
 
 }
